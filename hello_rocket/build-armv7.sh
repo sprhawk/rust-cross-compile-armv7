f56610c523871deceb4f0525c4281edabd896bb9
Yang Hongbo <465558+sprhawk@users.noreply.github.com>
2018-04-27 22:59:22 +0800

    a cross-buildable configuration for Rocket helloworld

--- hello_rocket/build-armv7.sh
+++ hello_rocket/build-armv7.sh
@@ -1,7 +1,9 @@
 SDKROOT="/home/hongbo/Developer/build-disk/rk-none/sysroots"
 SYSROOT="$SDKROOT/cortexa17hf-neon-vfpv4-rk-linux-gnueabi"
 
-export CC="$SDKROOT/x86_64-rksdk-linux/usr/bin/arm-rk-linux/arm-rk-linux-gcc"
-export CFLAGS="-mfloat-abi=hard --sysroot=$SYSROOT -I$SYSROOT/usr/include"
+export CC_armv7_unknown_linux_gnueabihf="$SDKROOT/x86_64-rksdk-linux/usr/bin/arm-rk-linux/arm-rk-linux-gcc"
+export CFLAGS_armv7_unknown_linux_gnueabihf="-mcpu=cortex-a17 -mfloat-abi=hard -mfpu=neon-vfpv4 -march=armv7ve --sysroot=$SYSROOT -I$SYSROOT/usr/include"
+export CARGO_TARGET="armv7-unknown-linux-gnueabihf"
+export CARGO_HOST="x86_64-unknown-linux-gnueabi"
 
-rustup run nightly cargo build  --target=armv7-unknown-linux-gnueabihf
+rustup run nightly cargo build --target armv7-unknown-linux-gnueabihf
